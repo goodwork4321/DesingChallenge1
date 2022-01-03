b376fc9ceaea9c8e59d5dfc7e5482e4f86f76dd3
goodwork4321 <65865028+goodwork4321@users.noreply.github.com>
2022-01-03 22:30:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-<script>
-  
-  </script>
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
